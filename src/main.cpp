f39f66bb20c73079a19aaf4a4270bc639c8bc6b9
phanthm <prathamtalware10@gmail.com>
2025-08-02 15:09:25 +0530

    dino class initialized

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include "raylib.h"
+#include "raymath.h"
+
+class Dino {
+private:
+int x, y;
+int width, height;
+Color color = BLACK;
+
+public:
+Dino(int x, int y, int width, int height) {
+    this->x = x;
+    this->y = y;
+    this->width = width;
+    this->height = height;
+}
+
+void Draw() {
+    DrawRectangle(x, y, width, height, color);
+}
+
+};
 
 int main(void) {
     const int screenWidth = 1920 * 0.75;
     const int screenHeight = 1080 * 0.75;
+
+    Dino dino(10, 10, 10, 10);
 
     InitWindow(screenWidth, screenHeight, "Dino Game!");
 
@@ -11,6 +34,7 @@ int main(void) {
     while (!WindowShouldClose()) {
         BeginDrawing();
 
+        dino.Draw();
         ClearBackground(RAYWHITE);
 
         DrawText("DINO GAME", 190, 200, 80, LIGHTGRAY);
